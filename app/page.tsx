71bb6a34b7566c9726dd43f7de0214b8f5f97d10
shvangiikumari475-wq <shivangiikumari475@gmail.com>
2026-06-20 11:22:40 +0530

    Fix input text color

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
               onChange={(e) => setSubject(e.target.value)}
               placeholder="e.g. Data Structures"
               required
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full border border-gray-300 rounded-lg px-3 py-2 text-gray-900  focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
@@ -79,7 +79,7 @@ export default function Home() {
               placeholder="e.g. Arrays, Linked List, Trees"
               required
               rows={3}
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full border border-gray-300 rounded-lg px-3 py-2  text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
@@ -92,7 +92,7 @@ export default function Home() {
               value={examDate}
               onChange={(e) => setExamDate(e.target.value)}
               required
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full border border-gray-300 rounded-lg px-3 py-2 text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
